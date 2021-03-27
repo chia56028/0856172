f2df5e20bac9dfdfd18e55664f53cc45be973787
chia56028 <chia56028@gmail.com>
2021-03-27 18:15:36 +0800

    fail case

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -15,7 +15,7 @@ public class PriorityQueueTest{
     static Stream<Arguments> StringIntAndListProvider() {
         return Stream.of(
                 //  test,correctAnswer
-                Arguments.of(new int[]{3,1,2}, new int[]{1,2,3}),
+                Arguments.of(new int[]{3,1,2}, new int[]{3,1,2}),
                 Arguments.of(new int[]{-3,-1,-2,5}, new int[]{-3,-2,-1,5}),
                 Arguments.of(new int[]{3,-2,-5,-1,2}, new int[]{-5,-2,-1,2,3}),
                 Arguments.of(new int[]{-3,1,11,0,9,3}, new int[]{-3,0,1,3,9,11}),
